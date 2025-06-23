7193bf639b8a8b16862f7251b1eff35f97ea043d
xkuubix <buler.jakub@gmail.com>
2025-06-23 13:21:32 +0200

    added 3-dataframes merging

--- file_manipulations.py
+++ file_manipulations.py
@@ -13,7 +13,7 @@ def read_excel(file_path, sheet_name=1):
     return data
 
 
-def make_metadata(path):
+def get_metadata(path):
     dcm_count = 0
     dcm_mlo_count = 0
     all_metadata = {}
@@ -54,7 +54,57 @@ def apply_exclusion_criteria(df_excel, all_metadata):
                 views['RMLO'] = ()
     totalLMLO = 0
     totalRMLO = 0
+    keys_to_delete = []
     for key in all_metadata.keys():
-        totalLMLO += len(all_metadata[key]['Laterality+View']['LMLO'])
-        totalRMLO += len(all_metadata[key]['Laterality+View']['RMLO'])
+        L = len(all_metadata[key]['Laterality+View']['LMLO'])
+        R = len(all_metadata[key]['Laterality+View']['RMLO'])
+        totalLMLO += L
+        totalRMLO += R
+        if L == 0 and R == 0:
+            keys_to_delete.append(key)
+    for key in keys_to_delete:
+        del all_metadata[key]
     print(f"Total DICOM files left after exclusion: {totalLMLO} LMLO, {totalRMLO} RMLO ({totalLMLO + totalRMLO} total)")
+
+
+def map_classification(classification):
+    cls = str(classification).lower()
+    if cls in ['benign', 'normal']:
+        return 'Negative'
+    elif cls == 'malignant':
+        return 'Positive'
+    else: # Invisible or Exclusion
+        return 'Unknown'
+    
+
+def merge_dataframes(df_images, df_annotations, df_excel):
+    
+    df_images_long = df_images.melt(
+        id_vars='ID', 
+        value_vars=['LMLO', 'RMLO'],
+        var_name='LeftRight',
+        value_name='ImagePath'
+    )
+    df_images_long = df_images_long[~df_images_long['ImagePath'].isin([[], ()])]
+    df_images_long['LeftRight'] = df_images_long['LeftRight'].map({'LMLO': 'L', 'RMLO': 'R'})
+    df_full = df_excel.merge(
+        df_images_long,
+        left_on=['ID', 'LeftRight'],
+        right_on=['ID', 'LeftRight'],
+        how='left'
+    )
+
+    df_positive = df_full[df_full['classificationMapped'] == 'Positive'].merge(
+        df_annotations,
+        on=['ID', 'LeftRight'],
+        how='left'
+    )
+
+    df_full = df_full.merge(
+        df_positive[['ID', 'LeftRight', 'Path']],
+        on=['ID', 'LeftRight'],
+        how='left'
+    )
+
+    df_full = df_full.dropna(subset=['ImagePath'])
+    return df_full
