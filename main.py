901853bbb99f271d3755c026fd3c14dcd1e0747f
xkuubix <buler.jakub@gmail.com>
2025-06-24 18:31:57 +0200

    moved code from main to make_df_and_generate_masks.py

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-#%%
-import matplotlib.pyplot as plt
-from file_manipulations import *
+# %%
+import pickle
+from TompeiDataset import TompeiDataset
+from torchvision import transforms as T
 
-if __name__ == "__main__":
-    file_path = '../metadata.csv'
-    excel_path = '../TOMPEI-CMMD_clinical_data_v01_20250121.xlsx'
-    path = '/users/project1/pt01190/TOMPEI-CMMD/CMMD-original/'
+df = pickle.load(open('/users/project1/pt01190/TOMPEI-CMMD/df_with_masks.pkl', 'rb'))
 
-    df_excel = read_excel(excel_path)
-    all_metadata = make_metadata(path)
-    apply_exclusion_criteria(df_excel, all_metadata)
+# TODO DATA SPLIT (by ID)
+# TODO TRANSFORMATIONS
 
-#%%
-# TODO visualize annotations
-    for patient_id, studies in all_metadata.items():
-        for study, images in studies.items():
-            for img_metadata in images:
-                ds = dcmread(img_metadata['FilePath'])
-                plt.imshow(ds.pixel_array, cmap='gray')
-                plt.title(f"PatientID: {patient_id}, View: {img_metadata['View']}, Laterality: {img_metadata['Laterality']}")
-                plt.show()
+train_dataset = TompeiDataset(df, transform=T.ToTensor())
 
 
 
-#%%
+
+# %%
