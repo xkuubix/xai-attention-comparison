34f79e20c0081eed5dc5e71baeca062e6a955dd6
xkuubix <buler.jakub@gmail.com>
2025-06-25 15:46:45 +0200

    code readability changes

--- utils.py
+++ utils.py
@@ -52,23 +52,37 @@ def random_split_df(df: DataFrame,
 def get_dataloaders(config):
     df = pd.read_pickle(config['data']['metadata_path'])
     seed = config['seed']
-    train_set, val_set, test_set = random_split_df(df,
-                                                   config['data']['fraction_train_rest'],
-                                                   config['data']['fraction_val_test'],
-                                                   seed=seed)
+    train_set, val_set, test_set = random_split_df(
+        df,
+        config['data']['fraction_train_rest'],
+        config['data']['fraction_val_test'],
+        seed=seed
+        )
+        # torchvision transforms expect PIL Images or torch.Tensor, not np.ndarray.
+        # To apply transforms on np.ndarray, first convert to PIL Image.
 
-    train_transforms = T.Compose([T.ToTensor(),
-                                  T.RandomAdjustSharpness(0.05, p=0.25),
-                                  T.RandomAutocontrast(p=0.25),
-                                  T.RandomEqualize(p=0.25),
-                                  T.RandomErasing(p=0.25, scale=(0.02, 0.12), ratio=(0.2, 2.2), value='random'),
-                                  ])
+    train_transforms = T.Compose([
+        T.ToPILImage(),
+        T.RandomAdjustSharpness(0.05, p=0.25),
+        T.RandomAutocontrast(p=0.25),
+        T.RandomEqualize(p=0.25),
+        T.ToTensor(),
+        T.RandomErasing(p=0.25, scale=(0.02, 0.12), ratio=(0.2, 2.2), value='random'),
+        ])
     val_test_transforms = T.Compose([T.ToTensor(),
                                      ])
-    train_dataset = TompeiDataset(train_set, transform=train_transforms, config=config)
-    val_dataset = TompeiDataset(val_set, transform=val_test_transforms, config=config)
-    test_dataset = TompeiDataset(test_set, transform=val_test_transforms, config=config)
-    
+    train_dataset = TompeiDataset(
+        train_set,
+        transform=train_transforms,
+        config=config)
+    val_dataset = TompeiDataset(
+        val_set,
+        transform=val_test_transforms,
+        config=config)
+    test_dataset = TompeiDataset(
+        test_set,
+        transform=val_test_transforms,
+        config=config)
     print("Class counts per set:")
     print(f"  Train set: {Counter(train_dataset.class_name)}")
     print(f"  Validation set: {Counter(val_dataset.class_name)}")
@@ -81,27 +95,30 @@ def get_dataloaders(config):
     g = torch.Generator()
     g.manual_seed(config['seed'])
 
-    train_loader = DataLoader(train_dataset,
-                              batch_size=config['training_plan']['parameters']['batch_size'],
-                              shuffle=True,
-                              num_workers=config['training_plan']['parameters']['num_workers'],
-                              worker_init_fn=seed_worker,
-                              generator=g
-                              )
-    val_loader = DataLoader(val_dataset,
-                            batch_size=config['training_plan']['parameters']['batch_size'],
-                            shuffle=False,
-                            num_workers=config['training_plan']['parameters']['num_workers'],
-                            worker_init_fn=seed_worker,
-                            generator=g
-                            )
-    test_loader = DataLoader(test_dataset,
-                             batch_size=config['training_plan']['parameters']['batch_size'],
-                             shuffle=False,
-                             num_workers=config['training_plan']['parameters']['num_workers'],
-                            worker_init_fn=seed_worker,
-                            generator=g
-                            )
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=config['training_plan']['parameters']['batch_size'],
+        shuffle=True,
+        num_workers=config['training_plan']['parameters']['num_workers'],
+        worker_init_fn=seed_worker,
+        generator=g
+        )
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=config['training_plan']['parameters']['batch_size'],
+        shuffle=False,
+        num_workers=config['training_plan']['parameters']['num_workers'],
+        worker_init_fn=seed_worker,
+        generator=g
+        )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=config['training_plan']['parameters']['batch_size'],
+        shuffle=False,
+        num_workers=config['training_plan']['parameters']['num_workers'],
+        worker_init_fn=seed_worker,
+        generator=g
+        )
     dataloaders = {'train': train_loader,
                    'val': val_loader,
                    'test': test_loader}
