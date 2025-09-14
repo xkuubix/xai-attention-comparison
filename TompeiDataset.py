449af1ff4f7760eb8ed3e7f86829f09fb8809bd6
xkuubix <buler.jakub@gmail.com>
2025-09-14 21:54:30 +0200

    Enhance TompeiDataset to support NIfTI masks and improve image loading logic

--- TompeiDataset.py
+++ TompeiDataset.py
@@ -8,6 +8,7 @@ import torch
 import torchvision.transforms.functional as TF
 from torchvision import transforms as T
 import logging
+import nibabel as nib
 logger = logging.getLogger(__name__)
 
 class TompeiDataset(Dataset):
@@ -28,13 +29,35 @@ class TompeiDataset(Dataset):
         return len(self.df)
 
     def __getitem__(self, idx):
-
         img_name = os.path.join(self.df.iloc[idx]['ImagePath'])
-        image = dcmread(img_name).pixel_array
-        if image.ndim == 2:  # If grayscale, convert to RGB
+        image = dcmread(img_name)
+        if image.BitsStored == 8: # ToTensor handles uint8
+            image = image.pixel_array
+        elif image.BitsStored == 12 or image.BitsStored == 16:
+            bits = image.BitsStored
+            max_val = float(2**bits - 1)
+            image = (image.pixel_array / max_val).astype(np.float32)
+        else:
+            raise ValueError(f"Unsupported BitsStored: {image.BitsStored}")
+        
+        if image.ndim == 2:
             image = np.stack((image,)*3, axis=-1)
-        mask_name = os.path.join(self.df.iloc[idx]['MaskPath'])
-        mask = Image.open(mask_name)
+
+        mask_path = self.df.iloc[idx]['MaskPath']
+        if os.path.exists(f"{mask_path}"):
+            if mask_path.endswith('.nii.gz'):
+                mask_nii = nib.load(mask_path)
+                mask = mask_nii.get_fdata()
+                mask = (mask > 0).astype(np.uint8)  # lesion mask to binary 
+                mask = Image.fromarray(mask)
+            else:
+                mask = Image.open(mask_path)
+        else:
+            mask = torch.zeros(image.shape[:2], dtype=torch.uint8)
+       
+        if not isinstance(mask, Image.Image):
+            mask = Image.fromarray(np.array(mask, dtype=np.uint8))
+
         label_str = self.df.iloc[idx]['ClassificationMapped']
         if self.config:
             if self.config['training_plan']['criterion'].lower() == 'bce':
@@ -45,20 +68,24 @@ class TompeiDataset(Dataset):
                 label = torch.tensor(label, dtype=torch.long)
 
         metadata = self.df.iloc[idx].to_dict()
-
+        if metadata['MaskPath'] == None:
+            metadata['MaskPath'] = ' '
         if self.transform:
-            # TODO add albumentation transformations
             image = self.transform(image)
-            pass
 
         if self.df.iloc[idx]['LeftRight'] == 'R':
             image = T.RandomHorizontalFlip(p=1.0)(image)
             mask = TF.hflip(mask)
 
         mask = np.array(mask, dtype=np.bool)
-        return {"image": image,
-                "target": {
-                    "mask": mask,
-                    "label": label,
-                },
-                "metadata": metadata}
+        # print(f"[DEBUG] idx={idx}, image={type(image)}, mask={type(mask)}, label={type(label)}, metadata={type(metadata)}")
+
+
+        return {
+            "image": image,
+            "target": {
+                "mask": mask,
+                "label": label,
+            },
+            "metadata": metadata
+        }
